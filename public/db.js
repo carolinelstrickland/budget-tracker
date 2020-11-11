91899bbbc82218c5374645ddd96cbcb64189f1e5
Caroline Strickland <carolinelstrickland@outlook.com>
2020-11-10 22:06:54 -0600

    handling request methods for db.js

--- public/db.js
+++ public/db.js
@@ -7,3 +7,27 @@ window.shimIndexedDB;
 
 let db;
 const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = ({ target }) => {
+    let db = target.result;
+    db.createdObjectStore("pending", { autoIncrement: true });
+};
+
+request.onsuccess = ({ target }) => {
+    db = target.result;
+    if (navigator.online) {
+        checkDatabase();
+    }
+};
+
+request.onerror = function(event) {
+    console.log("Whoops! " + event.target.errorCode);
+};
+
+function saveRecord(record) {
+    const transaction = db.transation(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+
+    store.add(record);
+}
+
