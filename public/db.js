d6c1d02224fb59f0d713212fddbeff4aca46f560
Caroline Strickland <carolinelstrickland@outlook.com>
2020-11-11 13:03:31 -0600

    fixing error in console for saveRecord function

--- public/db.js
+++ public/db.js
@@ -8,12 +8,12 @@ const indexedDB =
 let db;
 const request = indexedDB.open("budget", 1);
 
-request.onupgradeneeded = ({ target }) => {
-    let db = target.result;
-    db.createdObjectStore("pending", { autoIncrement: true });
+request.onupgradeneeded = function({ target }) {
+    const db = target.result;
+    db.createObjectStore("pending", { autoIncrement: true });
 };
 
-request.onsuccess = ({ target }) => {
+request.onsuccess = function({ target }) {
     db = target.result;
     if (navigator.onLine) {
         checkDatabase();
