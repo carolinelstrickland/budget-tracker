f4502dcfdb50f97e829495939dec6d51156b890b
Caroline Strickland <carolinelstrickland@outlook.com>
2020-11-10 22:14:55 -0600

    adding checkDatabase function to db.js

--- public/db.js
+++ public/db.js
@@ -1,3 +1,5 @@
+const { response } = require("express");
+
 const indexedDB = 
 window.indexedDB ||
 window.mozIndexedDB ||
@@ -31,3 +33,30 @@ function saveRecord(record) {
     store.add(record);
 }
 
+function checkDatabase(){
+    const transaction = db.transation(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+
+
+    getAll.onsuccess = function() {
+        if(getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain", "/",
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(response => {
+                return response.json();
+            })
+            .then(() => {
+                const transaction = db.transation(["pending"], "readwrite");
+                const store = transaction.objectStore("pending");
+                store.clear();
+            });
+            }
+        };
+}
